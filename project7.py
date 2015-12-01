e26d20fe400b83c9632370d05a84d9177dabc7f2
Dezmon Fernandez <defernan@mines.edu>
2015-12-01 00:22:59 -0700

    restaurants found

--- project7.py
+++ project7.py
@@ -1,23 +1,43 @@
-
 import getpass
 import pg8000
-
-
 
 #############################################################################
 # Functions
 #############################################################################
-def displayCities():
-    query = """SELECT id, name FROM airports"""
-    cursor.execute(query,)
+def pickArrivalLocation():
+    airport = input("Enter what airport you will be landing at(partial phrase works as well): ")
+    query = """SELECT id, name FROM airports
+                WHERE POSITION(LOWER(%s) IN LOWER(name)) > 0"""
+    cursor.execute(query,(str(airport),))
     results = cursor.fetchall()
     #print albums
     for row in results:
         ID = row[0]
         name = row[1]
         print(ID, "|", name)
-#############################################################################
+    if len(results) == 0:
+        print("\nSorry airport not found.\n")
+        pickAirport()
+    else:
+        airport_id = input("Type the ID number of your arrival location: ")
+        display_venues(airport_id)
 
+def display_venues(airport_id):
+    rating = input("Enter the min rating of the venue you'd like to visit(1-5): ")
+    query = """SELECT venue.name, venue.address, venue.stars FROM flight_data_restaurants as venue 
+               JOIN airports ON venue.city = airports.city WHERE airports.id = %s AND venue.stars > %s"""
+    cursor.execute(query, (airport_id, rating))
+    results = cursor.fetchall()
+    for row in results:
+        name = row[0]
+        address = row[1]
+        stars = row[2]
+        print(name, "|", address, "|", stars)
+    print("\n")
+    if len(results) == 0:
+        print("No results found.\n")
+
+#############################################################################
 #login = input('login: ')
 #secret = getpass.getpass('password: ')
 
@@ -40,8 +60,8 @@ except pg8000.Error as e:
 
 cursor = db.cursor()
 
-displayCities()
-
+while(True):
+    pickArrivalLocation()
 
 
 cursor.close()
